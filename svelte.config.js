b56fdc35f7e2d6442eef661b66518b7b72d5532d
Raja Gottumukkala <rajagottumukkala3@gmail.com>
2024-04-07 18:07:47 -0700

    amplify adapter added

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-node";
+// import adapter from "@sveltejs/adapter-node";
+import adapter from "amplify-adapter";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import dotenv from "dotenv";
 
